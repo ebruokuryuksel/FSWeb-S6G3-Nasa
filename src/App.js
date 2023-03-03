b8b63be32eb75c63d78b2f9c3bb1489948a7e339
Ebru Okur Yuksel <ebruokur3@gmail.com>
2023-03-03 13:15:51 +0300

    s6g4 nasa done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useEffect,useState } from "react";
 import axios from "axios";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
 function App() {
   const [info, setInfo] = useState(null); //neden yazdım?
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
   useEffect(() => {
     axios.get("https://api.nasa.gov/planetary/apod", {
       params: {
-        api_key: "DEMO_KEY",
-        date: date.toISOString().split("T")[0],
+        api_key: "waTxpksVjghFqmVLWQ4Mlgr5K2vcJGqmq7wD9IV1",
+        date: date,
       }
     })
     .then(function (response) {
@@ -25,16 +23,33 @@ function App() {
       // always executed
     });  
   }, [date])
+
+  if (!info)
   return (
     <div className="App">
+      <h1>Yükleniyor...</h1>
+    </div>
+  );
+
+  return (
+    <div className="App"
+    style = {{
+      width : "100vw",
+      height: "100vh",
+      backgroundImage: `url(${info.hdurl})`,
+    }}>
       {
         info && (
           <div className = "container">
-            <DatePicker selected={date} onChange={(e) => setDate(e)} />
+            <input 
+            type="date" 
+            value={date}
+            onChange={(event)  => setDate(event.target.value)} 
+            />
             <h1>{info.title}</h1>
-            <h1>{info.copyright}</h1>
-            <h1>{info.date}</h1>
-            <img src = {info.url} alt = {info.title}/>
+            <h2>{info.copyright}</h2>
+            <h3>{info.date}</h3>
+            {/* <img src = {info.url} alt = {info.title}/> */}
             </div>
         )
       }
